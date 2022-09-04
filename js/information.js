b98c5715f33bd11cb65c8fb491d16775362dcb50
senju96 <senju.t.e819570@gmal.com>
2022-09-04 15:20:24 +0900

    個人情報

---
+++ js/information.js
@@ -0,0 +1,28 @@
+const memberList = [
+  {'nameRome' : 'Yuki Nishida',
+    'nameKanji' : '西田優希',
+    'univ' : '早稲田大学', 
+    'depart' : '教育学部', 
+    'from' : '千葉県千葉市', 
+    'birthDay' : '11/7', 
+    'hobby' : [['スポーツ', '海外サッカー'], ['アウトドア', '海']]
+  },
+
+  {'nameRome' : 'Miaki Matayoshi',
+    'nameKanji' : '又吉見秋',
+    'univ' : '早稲田大学',
+    'depart' : '教育学部',
+    'from' : '沖縄県',
+    'birthDay' : null,
+    'hobby' : [['スポーツ', 'DeNA'], ['アニメ', 'HUNTER×HUNTER'], ['ゲーム', null]]
+  },
+
+  {'nameRome' : 'Haruna Nohara', 
+    'nameKanji' : '野原春菜', 
+    'univ' : '早稲田大学', 
+    'depart' : 'スポーツ科学部', 
+    'from' : '岩手県盛岡市', 
+    'birthDay' : '2/19', 
+    'hobby' : [['写真', null], ['スポーツ', '陸上'], ['食べ物', 'おいしい']]
+  },
+]
