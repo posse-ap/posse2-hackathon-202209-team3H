2236f91399622efec6530116de5eeed30d39c388
“Mayuko <sonyeye0824@gmail.cpm>
2022-09-06 07:20:14 +0900

    見た目の修正

--- js/scroll.js
+++ js/scroll.js
@@ -1,6 +1,8 @@
+'use strict';
+
 $('#page-link a[href*="#"]').click(function () {
-  var elmHash = $(this).attr('href'); //ページ内リンクのHTMLタグhrefから、リンクされているエリアidの値を取得
-  var pos = $(elmHash).offset().top-70;//idの上部の距離からHeaderの高さを引いた値を取得
-  $('body,html').animate({scrollTop: pos}, 1500); //取得した位置にスクロール。500の数値が大きくなるほどゆっくりスクロール
+  var elmHash = $(this).attr('href'); 
+  var pos = $(elmHash).offset().top-70;
+  $('body,html').animate({scrollTop: pos}, 1500); 
   return false;
 });
